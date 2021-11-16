934eb0d62a9376bf5144a2bb4d5e1c05e0aa3594
CallMeTwitch <63736686+CallMeTwitch@users.noreply.github.com>
2021-11-16 16:48:41 -0330

    Update Collatz_Conjecture.py

--- Collatz_Conjecture.py
+++ Collatz_Conjecture.py
@@ -21,22 +21,35 @@
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
-num = int(input('Starting Number: '))
+# Get Range to Check, Make Memory
+top_range = int(input('Top Range: '))
+mem = [0] * (top_range + 1)
 
-mem = [0]
+def collatz():
+    for start in tqdm(range(2, top_range + 1)):
+        # If mod(4) == 1: Value 2 or 3 Cached
+        if start % 4 == 1:
+            if ((3 * start) + 1) % 4:
+                mem[start - 1] = mem[(((3 * start) + 1)//2)] + 2
+            else:
+                mem[start] = mem[(((3 * start) + 1)//4)] + 3
+        # If mod(4) == 3: Use Algorithm
+        elif start % 4 == 3:
+            num = start
+            count = 0
+            while num >= start:
+                if num % 2:
+                    num = ((3 * num) + 1) // 2
+                    count += 2
+                else:
+                    num //= 2
+                    count += 1
+            mem[start] = mem[num] + count
+        # If mod(4) == 2 or 4: Value 1 Cached
+        else:
+            mem[start] = mem[(start // 2)] + 1
 
-# Edits mem in place, appending the length of each collatz sequence
-def collatz(num, count = 0):
-    if num < q:
-        mem.append(mem[num - 1] + count)
-    elif num % 2:
-        collatz(((3 * num) + 1) // 2, count + 2)
-    else:
-        collatz(num // 2, count + 1)
-
-# Fill mem
-for q in tqdm(range(2, num + 1)):
-    collatz(q)
+collatz()
 
 # Plot each starting number with the length of it's sequence
 plt.scatter([*range(1, len(mem) + 1)], mem, color = 'black', s = 1)
