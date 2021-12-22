cdc49243ad7b1c1b2110f9177b5eb3a10ec210b6
CallMeTwitch <63736686+CallMeTwitch@users.noreply.github.com>
2021-12-22 14:27:09 -0330

    Update Collatz_Conjecture.py

--- Collatz_Conjecture.py
+++ Collatz_Conjecture.py
@@ -18,39 +18,46 @@
 # efficiency.
 # #########################
 
+# Imports
 from matplotlib import pyplot as plt
-from tqdm import tqdm
+import numpy as np
+import numba as nb
 
-# Get Range to Check, Make Memory
+# Get Range to Check
 top_range = int(input('Top Range: '))
-mem = [0] * (top_range + 1)
 
-def collatz():
-    for start in tqdm(range(2, top_range + 1)):
-        # If mod(4) == 1: Value 2 or 3 Cached
+@nb.njit('int64[:](int_)')
+def collatz(top_range):
+    # Initialize mem
+    mem = np.zeros(top_range + 1, dtype = np.int64)
+    for start in range(2, top_range + 1):
+        # If mod4 == 1: (3x + 1)/4
         if start % 4 == 1:
-            if ((3 * start) + 1) % 4:
-                mem[start - 1] = mem[(((3 * start) + 1)//2)] + 2
-            else:
-                mem[start] = mem[(((3 * start) + 1)//4)] + 3
-        # If mod(4) == 3: Use Algorithm
+            mem[start] = mem[(start + (start >> 1) + 1) // 2] + 3
+        
+        # If 4mod == 3: (3(3x + 1)/2 + 1)/2 and continue
         elif start % 4 == 3:
-            num = start
-            count = 0
+            num = start + (start >> 1) + 1
+            num += (num >> 1) + 1
+            count = 4
+
             while num >= start:
                 if num % 2:
-                    num = ((3 * num) + 1) // 2
+                    num += (num >> 1) + 1
                     count += 2
                 else:
                     num //= 2
                     count += 1
             mem[start] = mem[num] + count
-        # If mod(4) == 2 or 4: Value 1 Cached
+
+        # If 4mod == 2 or 0: x/2
         else:
             mem[start] = mem[(start // 2)] + 1
 
-collatz()
+    return mem
 
-# Plot each starting number with the length of it's sequence
-plt.scatter([*range(1, len(mem) + 1)], mem, color = 'black', s = 1)
+mem = collatz(top_range)
+
+# Plot
+plt.scatter([*range(len(mem))], mem, color = 'black', s = 1)
 plt.show()
